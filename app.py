aa7b6c1861ef0ae91a6f413e5b8a2b860426d314
joseolinda <joseolindad@yahoo.com.br>
2023-05-25 09:19:48 -0300

    Sistema de Busca

--- app.py
+++ app.py
@@ -282,6 +282,13 @@ class User(db.Model):
     password = db.Column(db.String(255))
     img = db.Column(db.Text())
     status = db.Column(db.String(255))
+    
+class Message(db.Model):
+    __tablename__ = 'messages'
+    msg_id = db.Column(db.Integer, primary_key=True)
+    incoming_msg_id = db.Column(db.String(255))
+    outgoing_msg_id = db.Column(db.String(255))
+    msg = db.Column(db.String(1000))
 
 
 if __name__ == '__main__':
